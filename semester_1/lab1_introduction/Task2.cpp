b3b28698ad4dacd62fdb6d32ee62525add6df412
VladKoBY <VladKoBY@yandex.ru>
2025-09-09 11:02:01 +0300

    Modified task2

--- semester_1/lab1_introduction/Task2.cpp
+++ semester_1/lab1_introduction/Task2.cpp
@@ -2,16 +2,22 @@
 
 int main()
 {
-    int n, s, p;
+    int n, s, p, i;
     std::cin>>n;
     s=0;
     p=1;
-    for(n;n>=1;n--)
+    if(n<1){std::cout<<"Error. Enter right number"}
+else{
+    for(i=2;i<=n;i=i+2)
     {
-        s=s+n;
-        p=p*n;
+        s=s+i;
+    }
+    for(i=1;i<=n;i=i+2)
+    {
+        p=p*i;
     }
     std::cout<<"Сумма: "<<s<<std::endl;
     std::cout<<"Произведение: "<<p<<std::endl;
+}
     return 0;
 }
