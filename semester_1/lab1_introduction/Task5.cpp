e2e4e79def56d10c3334f27511665904431692fa
VladKoBY <VladKoBY@yandex.ru>
2025-09-15 20:42:36 +0300

    remake task5

--- semester_1/lab1_introduction/Task5.cpp
+++ semester_1/lab1_introduction/Task5.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 
+int nod(int a, int b);
+
 int main()
 {
-    unsigned int n, m, i;
-    std::cin>>n>>m;
-    std::cout<<"Общие делители:"<<std::endl;
-    for(i=1;i<=n;i++)
+    unsigned int n, m, Nod;
+    if((std::cin>>n>>m)&&(n)&&(m))
     {
-        if((!(n%i))&&(!(m%i)))
+        Nod = nod(n, m);
+        std::cout<<"Nod: "<<Nod<<std::endl;
+        std::cout<<"Dividers: ";
+        for(int i=1; i<=Nod; i++)
         {
-            std::cout<<i<<std::endl;
+            if(!(Nod%i))
+            {
+                std::cout<<i<<" ";
+            }
         }
+        return 0;
     }
-    return 0;
+    else
+    {
+        std::cout<<"Error. Enter right numbers"<<std::endl;
+        return 1;
+    }
+}
+
+int nod(int a, int b)
+{
+    do
+    {
+        if(a>b)
+        a=a%b;
+        else
+        b=b%a;
+    } while ((a!=0)&&(b!=0));
+    return a;
 }
