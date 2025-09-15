afed974c33764a302ce8a86a0ec63b38d8cdd453
VladKoBY <VladKoBY@yandex.ru>
2025-09-15 20:14:20 +0300

    modified Task4

--- semester_1/lab1_introduction/Task4.cpp
+++ semester_1/lab1_introduction/Task4.cpp
@@ -2,16 +2,22 @@
 
 int main()
 {
-    int k, f, i;
-    std::cin>>k;
-    f=1;
-    if(k%2) i=1;
-    else    i=2;
-
-    for(i;i<=k;i=i+2)
+    int k, f=1, i;
+    if((std::cin>>k)&&(k>=1))
     {
-    f=f*i;
+        if(k%2) i=1;
+        else    i=2;
+        
+        for(;i<=k;i+=2)
+        {
+            f=f*i;
+        }
+        std::cout<<"Fact: "<<f<<std::endl;
+        return 0;
     }
-    std::cout<<"Факториал: "<<f<<std::endl;
-    return 0;
+    else
+    {
+        std::cout<<"Error. Enter right number"<<std::endl;
+        return 1;
+    }
 }
