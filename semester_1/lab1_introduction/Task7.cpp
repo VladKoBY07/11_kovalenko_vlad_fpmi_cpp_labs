3eac5364151f6c191900d4321b7e08fa5096f94c
VladKoBY <VladKoBY@yandex.ru>
2025-09-15 21:02:35 +0300

    modified task7

--- semester_1/lab1_introduction/Task7.cpp
+++ semester_1/lab1_introduction/Task7.cpp
@@ -2,23 +2,29 @@
 
 int main()
 {
-    int n, i;
+    int n;
     double s, x, max, min;
-    std::cin>>n;
-    std::cin>>x;
-    max=x;
-    min=x;
-    s=x;
-    for(i=1;i<n;i++)
+    if(std::cin>>n>>x)
     {
-        std::cin>>x;
-        if(x<min) min=x;
-        if(x>max) max=x;
-        s=s+x;
+        max=x;
+        min=x;
+        s=x;
+        for(int i=1; i<n; i++)
+        {
+            std::cin>>x;
+            if(x<min) min=x;
+            if(x>max) max=x;
+            s=s+x;
+        }
+        s=s/n;
+        std::cout<<"Mid: "<<s<<std::endl;
+        std::cout<<"Min: "<<min<<std::endl;
+        std::cout<<"Max: "<<max<<std::endl;
+        return 0;
     }
-    s=s/n;
-    std::cout<<"Среднее арифметическое: "<<s<<std::endl;
-    std::cout<<"Минимальное число: "<<min<<std::endl;
-    std::cout<<"Максимальное число: "<<max<<std::endl;
-    return 0;
+    else
+    {
+        std::cout<<"Error. Enter right numbers"<<std::endl;
+        return 1;
+    }
 }
