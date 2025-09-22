3208634a4d4215cf0d6af4d788e06ca2c70b2d93
VladKoBY <VladKoBY@yandex.ru>
2025-09-22 20:59:46 +0300

    Update ISprime.cpp

--- semester_1/lab2_integer_arithmetics/ISprime.cpp
+++ semester_1/lab2_integer_arithmetics/ISprime.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
+#include <cmath>
+
+bool isprime(int x)
+{
+    int i, n;
+    bool flag;
+    if((x == 2) || (x == 3))
+    {
+        return true;
+    }
+    else
+    {
+        i = 2;
+        flag = 1;
+        n = sqrt(x);
+        while((i <= n) && (flag))
+            if(!(x%i))
+            {
+                flag = 0;
+            }
+            else
+            {
+                ++i;
+            }
+        return flag;
+    }
+}
 
 int main()
 {
-    int n;
-    std::cin>>n;
-    if (((n * n) % 24 == 1)||(n==2)||(n==3))
+    int a;
+    std::cin>>a;
+    if (isprime(a))
     {
-        std::cout<<"Prime";
+        std::cout<<"Prime"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"Typical"<<std::endl;
     }
     return 0;
 }
