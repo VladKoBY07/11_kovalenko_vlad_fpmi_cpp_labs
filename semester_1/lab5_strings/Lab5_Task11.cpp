5b630c7a9ba03d70f27c2251dbf6d70fa9712219
VladKoBY <VladKoBY@yandex.ru>
2025-11-03 20:25:26 +0300

    Lab5_solution1_every_letter

--- semester_1/lab5_strings/Lab5_Task11.cpp
+++ semester_1/lab5_strings/Lab5_Task11.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <string>
 
+bool IsLetter(char x)
+{
+    return (x >= 'A' && x <= 'Z')||(x >= 'a' && x <= 'z');
+}
+
+bool IsVovel(char x)
+{
+    return (x == 'a')||(x == 'A')||(x == 'e')||(x == 'E')||(x == 'i')||(x == 'I')
+    ||(x == 'o')||(x == 'O')||(x == 'u')||(x == 'U')||(x == 'y')||(x == 'Y');
+}
+
 int main()
 {
     try
@@ -13,7 +24,45 @@ int main()
             throw("Error. Input string is empty!");
         }
 
-        std::cout << "Your string:" << std::endl << inputStr;
+        size_t length = inputStr.length();
+        double MaxPercentage = -1;
+        std::string result;
+
+        for(size_t index = 0; index < length; ++index)
+        {
+            std::string word;
+            size_t numberOfVovels = 0, numberOfLetters = 0;
+            while(IsLetter(inputStr[index])&&(index < length))
+            {
+                word.push_back(inputStr[index]);
+                if(IsVovel(inputStr[index]))
+                {
+                    numberOfVovels++;
+                }
+                numberOfLetters++;
+                index++;
+            }
+            //std::cout << numberOfLetters << std::endl << numberOfVovels << std::endl;
+            //std::cout << word << std::endl << std::endl;
+            if(numberOfLetters > 0)
+            {
+                double percentage = numberOfVovels / numberOfLetters * 100;
+                if(percentage == MaxPercentage)
+                {
+                    result.push_back(' ');
+                    result.append(word);
+                }
+                if(percentage > MaxPercentage)
+                {
+                    MaxPercentage = percentage;
+                    result.clear();
+                    result.append(word);
+                }
+            }
+            word.clear();
+        }
+
+        std::cout << "Result:" << std::endl << result;
         return 0;
     }
     catch(const char * err)
