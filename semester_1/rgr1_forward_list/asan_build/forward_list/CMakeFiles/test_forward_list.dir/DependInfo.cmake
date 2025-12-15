786c1d68216a68d033bdbf399c657b20c520f4c6
VladKoBY <VladKoBY@yandex.ru>
2025-12-15 22:08:34 +0300

    Add testing folder

---
+++ semester_1/rgr1_forward_list/asan_build/forward_list/CMakeFiles/test_forward_list.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vladislav/Vlad/11_kovalenko_vlad_fpmi_cpp_labs/semester_1/rgr1_forward_list/forward_list/forward_list_impl.cpp" "forward_list/CMakeFiles/test_forward_list.dir/forward_list_impl.cpp.o" "gcc" "forward_list/CMakeFiles/test_forward_list.dir/forward_list_impl.cpp.o.d"
+  "/home/vladislav/Vlad/11_kovalenko_vlad_fpmi_cpp_labs/semester_1/rgr1_forward_list/forward_list/test.cpp" "forward_list/CMakeFiles/test_forward_list.dir/test.cpp.o" "gcc" "forward_list/CMakeFiles/test_forward_list.dir/test.cpp.o.d"
+  "" "test_forward_list" "gcc" "forward_list/CMakeFiles/test_forward_list.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
