34c88b6308e40c8a2f44dabf701ea6c003f03a6a
VladKoBY <VladKoBY@yandex.ru>
2025-12-21 17:58:07 +0300

    Add basic functions to trains

--- semester_1/rgr2_vector_stl/part2/main.cpp
+++ semester_1/rgr2_vector_stl/part2/main.cpp
@@ -2,8 +2,12 @@
 #include "train.h"
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
-int main() {
+// time_utility test?
+/*int main() {
 
     using namespace time_utility;
 
@@ -31,4 +35,100 @@ int main() {
 
 
     return EXIT_SUCCESS;
+}*/
+
+std::vector<Train> ReadFile(const std::string& inputfile)
+{
+    std::vector<Train> all_trains;
+    std::ifstream instream(inputfile);
+
+    if(!(instream.is_open()))
+    {
+        throw "Error! Cannot open the file!";
+    }
+
+    std::string line;
+    while(std::getline(instream, line))
+    {
+        std::istringstream lineStream(line);
+        TrainId id;
+        if(!(lineStream >> id))
+        {
+            throw "Error! Wrong train id detected";
+        }
+        std::string string_type, destination;
+        if(!(lineStream >> string_type))
+        {
+            throw "Error! Wrong train type detected";
+        }
+        if(!(lineStream >> destination))
+        {
+            throw "Error! Wrong train destination detected";
+        }
+
+        size_t disp_hours, disp_minutes, travel_hours, travel_minutes;
+        if(!(lineStream >> disp_hours >> disp_minutes))
+        {
+            throw "Error! Wrong data in dispatch time detected!";
+        }
+        if(!(lineStream >> travel_hours >> travel_minutes))
+        {
+            throw "Error! Wrong data in travel time detected!";
+        }
+
+        // преобразование данных
+        TrainType type = Train::Select_train_type(string_type);
+        std::time_t disp_time = time_utility::SetTime(disp_hours, disp_minutes);
+        std::time_t travel_time = time_utility::SetTime(travel_hours, travel_minutes);
+
+        Train new_train(id, type, destination, disp_time, travel_time);
+        all_trains.push_back(new_train);
+    }
+
+    instream.close();
+    return all_trains;
+}
+
+void PrintList(const std::vector<Train>& trains_list)
+{
+    for(int i = 0; i < trains_list.size(); ++i)
+    {
+        PrintTrainInfo(trains_list[i]);
+    }
+}
+
+int main()
+{
+    try
+    {
+        std::cout << "---Программа для управления расписанием поездов---" << std::endl;
+        std::cout << "--------------------------------------------------";
+
+        std::string inputfile = "Input.txt";
+        std::cout << "Чтение файла " << inputfile << " ..." << std::endl;
+        std::vector<Train> trains_list;
+        trains_list = ReadFile(inputfile);
+
+        if(trains_list.empty())
+        {
+            throw "Error! Failed to upload data";
+        }
+
+        std::cout << "Успешно загружено " << trains_list.size() << " поездов!" << std::endl;
+        std::cout << "--------------------------------------------------";
+
+        std::cout << "Сортировка поездов по времени отправления поездов:";
+        PrintList(trains_list);
+
+    }
+    catch(const char* msg)
+    {
+        std::cerr << msg << std::endl;
+        return 1;
+    }
+    catch(...)
+    {
+        std::cerr << "Unknown error!" << std::endl;
+        return 1;
+    }
 }
--- semester_1/rgr2_vector_stl/part2/train.h
+++ semester_1/rgr2_vector_stl/part2/train.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 #include "time_utility.h"
 
 using TrainId = size_t;
@@ -13,13 +14,110 @@ enum class TrainType {
 
 
 class Train {
+    friend void PrintTrainInfo(const Train&);
 private:
-    TrainId id_;
-    TrainType type_;
-    std::string destination_;
-    std::time_t dispatch_time_;
-    std::time_t travelling_time_;
+    TrainId id_;   // номер поезда
+    TrainType type_;   // тип поезда
+    std::string destination_;   // название пункта назначения
+    std::time_t dispatch_time_;   // время отправления
+    std::time_t travelling_time_;   // время в пути
 
 public:
-    // your code goes here
+    Train() = default;
+
+    Train(TrainId id, TrainType type, const std::string& destination, std::time_t disp_time, std::time_t travel_time):
+    id_(id), type_(type), destination_(destination), dispatch_time_(disp_time), travelling_time_(travel_time) {}
+
+    // Геттеры всех полей
+    TrainId Get_id() const
+    {
+        return id_;
+    }
+
+    TrainType Get_type() const
+    {
+        return type_;
+    }
+
+    std::string Get_destination() const
+    {
+        return destination_;
+    }
+
+    std::time_t Get_dispatch() const
+    {
+        return dispatch_time_;
+    }
+
+    std::time_t Get_travelling() const
+    {
+        return travelling_time_;
+    }
+
+    // Ввод типа поезда
+    static TrainType Select_train_type(const std::string& strtype)
+    {
+        //PASSENGER, FREIGHT, HIGH_SPEED, SUBWAY, SPECIALIZED
+        if((strtype == "PASSENGER")||(strtype == "Passenger")||(strtype == "passenger"))
+        {
+            return TrainType::PASSENGER;
+        }
+        if((strtype == "FREIGHT")||(strtype == "Freight")||(strtype == "freight"))
+        {
+            return TrainType::FREIGHT;
+        }
+        if((strtype == "HIGH_SPEED")||(strtype == "High_speed")||(strtype == "high_speed"))
+        {
+            return TrainType::HIGH_SPEED;
+        }
+        if((strtype == "SUBWAY")||(strtype == "Subway")||(strtype == "subway"))
+        {
+            return TrainType::SUBWAY;
+        }
+        if((strtype == "SPECIALIZED")||(strtype == "Specialized")||(strtype == "specialized"))
+        {
+            return TrainType::SPECIALIZED;
+        }
+        return TrainType::SPECIALIZED;
+    }
 };
+
+// печать информации о поезде
+    void PrintTrainInfo(const Train& single_train)
+    {
+        std::cout << "Поезд №" << single_train.id_;
+        std::string lable;
+        switch (single_train.type_)
+        {
+        case TrainType::PASSENGER:
+            lable = "Пассажирский";
+            break;
+        
+        case TrainType::FREIGHT:
+            lable = "Грузовой";
+            break;
+
+        case TrainType::HIGH_SPEED:
+            lable = "Высокоскоростной";
+            break;
+
+        case TrainType::SUBWAY:
+            lable = "Подземный";
+            break;
+
+        case TrainType::SPECIALIZED:
+            lable = "Специальный";
+            break;
+
+        default:
+            lable = "Не определен";
+            break;
+        }
+
+        std::cout << " | " << "Тип: " << lable;
+        std::cout << " | " << "Пункт назначения: " << single_train.destination_;
+        std::cout << " | " << "Время отправления: ";
+        PrintTime(single_train.dispatch_time_);
+        std::cout << " | " << "Время в пути: ";
+        PrintTime(single_train.travelling_time_);
+    }
